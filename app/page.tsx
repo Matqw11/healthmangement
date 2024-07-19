b802b1d968e0a5aa47335815d59e60e97bf1b810
Matqw11 <kasulemtthw330@gmail.com>
2024-07-19 12:45:05 +0300

    added the patient.tsx changed the favicon and created the form for the user to signin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import PatientForm from "@/components/forms/patient";
+import Image from "next/image";
+import Link from "next/link";
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-3xl underline">Home</h1>
-   </div>
-  );
+   <div className="flex h-screen max-h-screen">
+        <section className="remove-scrollbar container my-auto">
+          {/*verification otp todo*/}
+      <div className="sub-container max-w-width-[496px]">
+        <Image 
+          src="/assets/images/file3.png"
+          height={5000}
+          width={1000}
+          alt="MattCareLogo"
+          className="mb-12 h-12 w-fit"
+        />
+        <PatientForm/>
+        <div className="text-14-regular mt-20 flex justify-between">
+          <p className="justify-items-end text-dark-600 xl:text-left">
+            © 2024 Matt Care
+          </p>
+          <Link href="/?admin=true" className="text-green-500">
+            Admin
+          </Link>
+        </div>
+      </div>
+    </section>
+    <Image 
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="Onboarding"
+      className="side-img max-w-[50%]"
+    />
+    </div>
+  )
 }  
